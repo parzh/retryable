cb9549778f77bea350340999159dc11929e2e4a5
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-28 19:46:10 +0200

    Wrap tests in describe()

--- index.spec.ts
+++ index.spec.ts
@@ -1,21 +1,23 @@
 import retryable from "./index";
 
-it("provides resolver, that works like Promise.resolve()", async () => {
-	const value = await retryable((resolve) => {
-		resolve(42);
-	});
-
-	expect(value).toBe(42);
-});
-
-it("provides rejecter, that works like Promise.reject()", async () => {
-	try {
-		await retryable((_resolve, reject) => {
-			reject("Unexpected error");
+describe("retryable()", () => {
+	it("provides resolver, that works like Promise.resolve()", async () => {
+		const value = await retryable((resolve) => {
+			resolve(42);
 		});
 
-		fail("Function did not throw");
-	} catch (error) {
-		expect(error).toContain("Unexpected error");
-	}
+		expect(value).toBe(42);
+	});
+
+	it("provides rejecter, that works like Promise.reject()", async () => {
+		try {
+			await retryable((_resolve, reject) => {
+				reject("Unexpected error");
+			});
+
+			fail("Function did not throw");
+		} catch (error) {
+			expect(error).toContain("Unexpected error");
+		}
+	});
 });
