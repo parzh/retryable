78d369f3f41aa5c7d0e8749ee6e315aba2b92d7a
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-31 01:13:24 +0200

    Cover assigned exports with unit tests
    TypeScript-only syntax, no tests for CommonJS

---
+++ src/__tests__/assigned-exports.spec.ts
@@ -0,0 +1,27 @@
+import retryable = require("..");
+
+/** @private */
+const ___ = typeof (null as any);
+
+/** @private */
+type TypeOf = typeof ___;
+
+describe('import retryable = require("..")', () => {
+	it("imports the whole module", () => {
+		expect(retryable).toHaveProperty("retryable");
+		expect(retryable).toHaveProperty("wait");
+		expect(retryable).toHaveProperty("default");
+	});
+
+	it("is callable (delegates to the `retryable` function)", async () => {
+		expect(typeof retryable).toBe<TypeOf>("function");
+
+		const value = await retryable<42>((resolve) => {
+			resolve(42);
+		});
+
+		expect(value).toBe(42);
+	});
+
+	it.todo("and looks ugly, but that works without tests");
+});
