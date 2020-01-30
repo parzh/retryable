10ff53ed227ac66fb6adc8197e1d1e6510642fef
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-31 00:42:25 +0200

    Fix default exports in CommonJS modules
    This will allow importing both `retryable` and `wait` with the following syntaxes:
    
      // ugly "default" import
      import retryable = require("@parzh/retryable");
    
      // prettier, but most likely requires "esModuleInterop": true (untested)
      import retryable from "@parzh/retryable";
    
      // gets `wait` function alone
      import { wait } from "@parzh/retryable";
    
      // gets `retryable` function alone
      import { retryable } from "@parzh/retryable";
    
      // (CommonJS) gets whole module, which is callable, - delegates to retryable
      const retryable = require("@parzh/retryable");
    
      // (CommonJS) gets the retryable function alone
      const retryable = require("@parzh/retryable").default;
    
      // (CommonJS) gets the retryable function alone
      const { retryable } = require("@parzh/retryable");
    
      // (CommonJS) gets the `wait` function alone
      const { wait } = require("@parzh/retryable");

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-export { default as default } from "./retryable";
-export { default as wait } from "./wait";
+import retryable from "./retryable";
+import wait from "./wait";
+
+/** @private */
+type RetryableFunction = typeof retryable;
+
+/** @private */
+type WaitFunction = typeof wait;
+
+/** @private */
+interface RetryableModule extends RetryableFunction {
+	retryable: RetryableFunction;
+	wait: WaitFunction;
+	default: RetryableFunction;
+}
+
+/** @private */
+const retryableCloned: RetryableFunction = (...args) => retryable(...args);
+
+/** @public */
+const _: RetryableModule = Object.assign(retryableCloned, {
+	retryable,
+	wait,
+	default: retryable,
+});
+
+export = _;
