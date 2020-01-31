217eb54cd522d3ec1033f1231348f777ad840bd5
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-31 10:44:48 +0200

    Refactor CommonJS imports tests a bit
    This will be useful in the next commit

--- src/__tests__/commonjs-imports.spec.js
+++ src/__tests__/commonjs-imports.spec.js
@@ -1,24 +1,23 @@
 // @ts-check
-const { retryable } = require("..");
-const { wait } = require("..");
+const module_ = require("..");
 
 describe("CommonJS tests", () => {
 	describe('const { retryable } = require("@parzh/retryable")', () => {
 		it("imports the `retryable` function itself", () => {
-			expect(typeof retryable).toBe("function");
+			expect(typeof module_.retryable).toBe("function");
 		});
-	
+
 		it("without importing the whole module", () => {
 			// @ts-ignore
 			const subject = retryable.wait;
-	
+
 			expect(subject).toBeUndefined();
 		});
 	});
-	
+
 	describe('const { wait } = require("@parzh/retryable")', () => {
 		it("imports the `wait` function itself", () => {
-			expect(typeof wait).toBe("function");
+			expect(typeof module_.wait).toBe("function");
 		});
 	});
 });
