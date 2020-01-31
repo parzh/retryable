68a6fc4751f74ad875ed31d1bddd15907c467004
Dima Parzhitsky <parzhitsky@gmail.com>
2020-01-31 10:50:21 +0200

    Merge al CommonJS tests to a single file

--- src/__tests__/commonjs-imports.spec.js
+++ src/__tests__/commonjs-imports.spec.js
@@ -1,23 +1,44 @@
 // @ts-check
 const module_ = require("..");
 
-describe("CommonJS tests", () => {
-	describe('const { retryable } = require("@parzh/retryable")', () => {
-		it("imports the `retryable` function itself", () => {
-			expect(typeof module_.retryable).toBe("function");
-		});
-
-		it("without importing the whole module", () => {
-			// @ts-ignore
-			const subject = retryable.wait;
-
-			expect(subject).toBeUndefined();
-		});
+describe('const retryable = require("@parzh/retryable")', () => {
+	it("imports the whole module", () => {
+		expect(module_).toHaveProperty("retryable");
+		expect(module_).toHaveProperty("wait");
+		expect(module_).toHaveProperty("default");
 	});
 
-	describe('const { wait } = require("@parzh/retryable")', () => {
-		it("imports the `wait` function itself", () => {
-			expect(typeof module_.wait).toBe("function");
+	it("is callable (delegates to the `retryable` function)", async () => {
+		expect(typeof module_).toBe("function");
+
+		const value = await module_((resolve) => {
+			resolve(42);
 		});
+
+		expect(value).toBe(42);
+	});
+});
+
+describe('const retryable = require("@parzh/retryable").default', () => {
+	it("imports the `retryable` function itself", () => {
+		expect(typeof module_.default).toBe("function");
+	});
+
+	it("without importing the whole module", () => {
+		expect(module_.default).not.toHaveProperty("retryable");
+		expect(module_.default).not.toHaveProperty("wait");
+		expect(module_.default).not.toHaveProperty("default");
+	});
+});
+
+describe('const { retryable } = require("@parzh/retryable")', () => {
+	it("imports the `retryable` function itself", () => {
+		expect(typeof module_.retryable).toBe("function");
+	});
+});
+
+describe('const { wait } = require("@parzh/retryable")', () => {
+	it("imports the `wait` function itself", () => {
+		expect(typeof module_.wait).toBe("function");
 	});
 });
