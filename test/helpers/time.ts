632349cd9fc2674541533bc0742f921b130d6589
Dima Parzhitsky <parzhitsky@gmail.com>
2020-02-01 12:43:08 +0200

    Enrich JSDoc for time() test-helper

--- test/helpers/time.ts
+++ test/helpers/time.ts
@@ -3,7 +3,26 @@ export const SECOND = 1000;
 /** @private */
 const PRECISION = 10 * SECOND;
 
-export const time = (msec?: number): number => (msec ?? Date.now()) / PRECISION;
+/**
+ * Returns the current time in test-friendly format
+ * @uses PRECISION constant internally
+ * @example
+ * time(); => 158055352.1882
+ */
+export function time(): number;
+
+/**
+ * Converts the supplied number of milliseconds
+ * to the test-friendly format
+ * @uses PRECISION constant internally
+ * @example
+ * time(300); => 0.03
+ */
+export function time(msec: number): number;
+
+export function time(msec?: number): number {
+	return (msec ?? Date.now()) / PRECISION;
+}
 
 /** Wait time for an arbitrary test */
 export const WAIT_TIME = 300;
