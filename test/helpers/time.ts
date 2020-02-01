11ec8017e6bdbc6cef60a0232981d85076b41242
Dima Parzhitsky <parzhitsky@gmail.com>
2020-02-01 12:45:30 +0200

    Put constants' exports to the top

--- test/helpers/time.ts
+++ test/helpers/time.ts
@@ -1,5 +1,10 @@
 export const SECOND = 1000;
 
+/** Wait time for an arbitrary test */
+export const WAIT_TIME = 300;
+
+export const TIMEOUT_MARGIN = 100;
+
 /** @private */
 const PRECISION = 10 * SECOND;
 
@@ -25,8 +30,3 @@ export default function time(msec?: number): number {
 }
 
 export { time };
-
-/** Wait time for an arbitrary test */
-export const WAIT_TIME = 300;
-
-export const TIMEOUT_MARGIN = 100;
