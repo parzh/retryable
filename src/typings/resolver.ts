1c9a25f009f9c903fb172e86413bd071c04043e1
Dima Parzhitsky <parzhitsky@gmail.com>
2020-02-02 13:30:53 +0200

    Fix some lint errors

--- src/typings/resolver.ts
+++ src/typings/resolver.ts
@@ -2,7 +2,7 @@ import PromiseConstructorParameters from "./promise-constructor-parameters";
 
 /** @deprecated Consider using `Resolver<unknown>` instead */
 export type ResolverLegacy =
-	| PromiseConstructorParameters[0];
+	| PromiseConstructorParameters[0]
 	;
 
 export default interface Resolver<Value = unknown> {
