de37225f0888df38155a31230c148c2b92bfacdd
Dima Parzhitsky <parzhitsky@gmail.com>
2020-02-04 14:03:16 +0200

    Fix the script
    First filter the labels, then check their number

--- scripts/check-labels.py
+++ scripts/check-labels.py
@@ -8,11 +8,11 @@ def is_change(label):
 	return label.startswith(LABEL_KEY + LABEL_SEP)
 
 labels = json.loads(os.environ['PR_LABELS'])
-
-if (count := len(labels)) != 1:
-	raise Exception('Expected exactly one change label, got %i' % count)
-
 label, *other = filter(is_change, labels)
+
+if count := len(other):
+	raise Exception('Expected exactly one change label, got %i' % (count + 1))
+
 key, value = label.split(LABEL_SEP)
 
 print('SemVer change is %s' % value)
