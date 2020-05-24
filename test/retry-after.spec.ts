21ea18ac3212708d3ab20ba196bfb5ad81da13f8
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-24 12:00:33 +0300

    Add unit test for 'exponential' delay

--- test/retry-after.spec.ts
+++ test/retry-after.spec.ts
@@ -52,3 +52,28 @@ describe("retry.after()", () => {
 		return expect(promise).resolves.toBeUndefined();
 	});
 });
+
+describe("named delays", () => {
+	it("properly delays retries if 'exponential' delay is provided", async () => {
+		const start = time();
+		const times: number[] = [];
+		const RETRY_LIMIT = 4;
+
+		await retryable((resolve, reject, retry) => {
+			times.push(time());
+
+			if (retry.count >= RETRY_LIMIT)
+				return resolve();
+
+			retry.after("exponential");
+		});
+
+		expect(times[0] - start).toBeCloseTo(time(0));
+		expect(times[1] - start).toBeCloseTo(time(0) + time(100));
+		expect(times[2] - start).toBeCloseTo(time(0) + time(100) + time(200));
+		expect(times[3] - start).toBeCloseTo(time(0) + time(100) + time(200) + time(400));
+		expect(times[4] - start).toBeCloseTo(time(0) + time(100) + time(200) + time(400) + time(800));
+
+		expect(times).toHaveLength(5);
+	}, TIMEOUT_MARGIN + 800 + 400 + 200 + 100);
+});
